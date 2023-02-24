53fdf9f48e21942e60d7eaa26cbca5096ff5c26e
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-02-24 22:06:30 +0800

    Adds a custom state store for the client

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
+import * as store from "./store.js";
 const socket = io("/");
+
 socket.on("connect", () => {
   console.log("Successfully connected to WS server via", socket.id);
+  store.setSocketId(socket.id);
 });
