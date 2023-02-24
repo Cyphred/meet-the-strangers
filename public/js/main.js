2c89cfd8f8ae60be40c07e899801167a8a59f4fe
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-02-24 23:45:31 +0800

    Moves wss functionality into the main client script

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,23 @@
 import * as store from "./store.js";
 const socket = io("/");
 
-socket.on("connect", () => {
-  console.log("Successfully connected to WS server via", socket.id);
-  store.setSocketId(socket.id);
+export const registerSocketEvents = (socket) => {
+  socket.on("connect", () => {
+    console.log("Successfully connected to WS server via", socket.id);
+    store.setSocketId(socket.id);
+  });
+};
+
+// Initializtion of socketIO connection
+const socket = io("/");
+registerSocketEvents(socket);
+
+// Registers event for personal code copy button
+const personalCodeCopyButton = document.getElementById(
+  "personal_code_copy_button"
+);
+personalCodeCopyButton.addEventListener("click", () => {
+  const personalCode = store.getState().socketId;
+  navigator.clipboard && navigator.clipboard.writeText(personalCode);
+  alert("Your perosnal code has been copied to the clipboard!");
 });
