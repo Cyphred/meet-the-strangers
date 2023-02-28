d5b376464c10174a6052c9e0fe921b110c1fe73e
Jeremy Andrews Zantua <tmq.jeremy.z@gmail.com>
2023-02-28 23:15:29 +0800

    Adds negotiation of ICE candidates and appropriate event listeners
    - Adds the forwarding of the received media stream to the UI

--- public/js/webRTCHandler.js
+++ public/js/webRTCHandler.js
@@ -2,11 +2,15 @@ import * as store from "./store.js";
 import * as constants from "./constants.js";
 import * as ui from "./ui.js";
 
+let peerConnection;
 const defaultConstraints = {
   audio: true,
   video: true,
 };
-let peerConnection;
+
+const configuration = {
+  iceServers: [{ urls: "stun:stun.l.google.com:13902" }],
+};
 
 export const getLocalPreview = () => {
   // Gets user media devices
@@ -25,4 +29,23 @@ export const getLocalPreview = () => {
 
 const createPeerConnection = () => {
   peerConnection = new RTCPeerConnection(configuration);
+  peerConnection.onicecandidate = (event) => {
+    if (event.candidate) {
+      // Send our ICE candidates to peer
+    }
+  };
+  peerConnection.onconnectionstatechange = (event) => {
+    if (peerConnection.connectionState === "connected") {
+      console.log("Successfully connected with peer");
+    }
+  };
+
+  // Receiving tracks
+  const remoteStream = new MediaStream();
+  store.setRemoteStream(remoteStream);
+  ui.updateRemoteVideo(remoteStream);
+
+  peerConnection.ontrack = (event) => {
+    remoteStream.addTrack(event.track);
+  };
 };
